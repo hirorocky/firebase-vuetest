49ab0f3a552a843503a1d62baf4d4d368d25e36a
Hiroki Nagasawa <hirorocky@gmail.com>
2019-06-08 19:11:22 +0900

    add signin page

--- src/main.js
+++ src/main.js
@@ -2,8 +2,21 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import firebase from "firebase";
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9dyUZGeGVOzzVgzCXrhLDVAm_wdzhJ0k",
+  authDomain: "vue-test-629a1.firebaseapp.com",
+  databaseURL: "https://vue-test-629a1.firebaseio.com",
+  projectId: "vue-test-629a1",
+  storageBucket: "vue-test-629a1.appspot.com",
+  messagingSenderId: "1026961178022",
+  appId: "1:1026961178022:web:e6eda4fa2be417b7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
