8b766176ce079409cf7b0616d806844c6bba725b
Hiroki Nagasawa <hirorocky@gmail.com>
2019-06-08 19:11:22 +0900

    add signin page

---
+++ src/views/SignIn.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="sign-in">
+    <h1 class="title is-5">Sign in</h1>
+    <div id="firebaseui-auth-container"></div>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+import "firebase/auth";
+import firebaseui from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
+
+export default {
+  mounted() {
+    let ui = firebaseui.auth.AuthUI.getInstance();
+    if (!ui) {
+      ui = new firebaseui.auth.AuthUI(firebase.auth());
+    }
+    ui.start("#firebaseui-auth-container", {
+      signInFlow: "redirect",
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      signInSuccessUrl: "/"
+    });
+  }
+};
+</script>
